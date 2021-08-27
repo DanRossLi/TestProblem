2d7ce815d28dc543085ac8ebb08412813f3f98cf
Shawn Witt <scwitt01@louisville.edu>
2021-08-27 15:29:24 -0400

    Test Problems 1 and 2 w/ argparse

--- TestProblem1.py
+++ TestProblem1.py
@@ -3,14 +3,18 @@ import io
 import argparse
 
 parser=argparse.ArgumentParser('csv')
-parser.add_argument('csv')
-args = parser.parse_args()
+parser.add_argument('csv', help="Specify CSV file to take data from")
 
-data = pd.read_csv(args.csv)
-total = 0
-df = pd.DataFrame(data)
-print(df)
-df['Resale Value'] = pd.to_numeric(df['Resale Value'].replace('[^0-9\.]', '', regex=True)).astype(float)
-total = df['Resale Value'].sum()
-currency = "${:,.2f}".format(total)
-print("Total Resale Value:" +currency)
+def ttlResale(csv):
+    data = pd.read_csv(csv)
+    total = 0
+    df = pd.DataFrame(data)
+    print(df)
+    df['Resale Value'] = pd.to_numeric(df['Resale Value'].replace('[^0-9\.]', '', regex=True)).astype(float)
+    total = df['Resale Value'].sum()
+    currency = "${:,.2f}".format(total)
+    print("Total Resale Value:" +currency)
+ 
+if __name__ == "__main__":       
+    args = parser.parse_args()
+    ttlResale(args.csv)
--- TestProblem2.py
+++ TestProblem2.py
@@ -1,13 +1,18 @@
 import pandas as pd
-import requests
 import io
+import argparse
 
-url = "https://raw.githubusercontent.com/TheRealSeat/TestProblem/main/TestData.csv?token=AS6K546XAD4DBV44RTXRFODBF6A7E"
-download = requests.get(url).content
+parser=argparse.ArgumentParser('csv')
+parser.add_argument('csv', help="Specify CSV file to take data from")
 
-data = pd.read_csv(io.StringIO(download.decode('utf-8')))
-df = pd.DataFrame(data, columns = ['Item', 'Resale Value'])
-df['Resale Value'] = pd.to_numeric(df['Resale Value'].replace('[^0-9\.]', '', regex=True)).astype(float)
-pd.options.display.float_format = '${:,.2f}'.format
-grouped = df.groupby('Item')['Resale Value'].sum().reset_index()
-print(grouped)
+def allResale(csv):
+    data = pd.read_csv(csv)
+    df = pd.DataFrame(data, columns = ['Item', 'Resale Value'])
+    df['Resale Value'] = pd.to_numeric(df['Resale Value'].replace('[^0-9\.]', '', regex=True)).astype(float)
+    pd.options.display.float_format = '${:,.2f}'.format
+    grouped = df.groupby('Item')['Resale Value'].sum().reset_index()
+    print(grouped)
+
+if __name__ == "__main__":       
+    args = parser.parse_args()
+    allResale(args.csv)
