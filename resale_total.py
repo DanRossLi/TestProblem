a773835ca1b89588c7b99fc5fd42fb466b99f941
Lucian Moore <lucian.moore64@gmail.com>
2021-09-17 13:41:45 -0400

    Streamlining
    Removed unnecessary lines, added a few results to the program's print() function.

--- resale_total.py
+++ resale_total.py
@@ -1,30 +1,16 @@
-import pandas as pd #tried to keep things as compact as possible once i finally got the packages to work!
-
+import pandas as pd 
 # -------------------CLEANING THE DATA
-
-#Removes duplicate entries from the csv file
-data = pd.read_csv("TestData.csv", delimiter=",")
+data = pd.read_csv("TestData.csv", delimiter=",") #Removes duplicate entries from the csv file
 data = data.drop_duplicates() 
 data.to_csv("TestData.csv", index=False)
-
-# -------------------WORKING THE DATA
-#this section is for finding the specific answers and outputting them to files, as requested/suggested by the README.md
-
-data["Resale Value"] = data["Resale Value"].str.replace('$','').str.replace(',','').astype(float)
-#removing the formatting $ signs and commas to make summing easier
-
-total = data["Resale Value"].sum() #sums total resale values 
-
-with open('TotalResale.txt', 'w') as f: 
-    f.write(str(total))
-
-dataItems = data[["Item", "Resale Value"]].groupby(["Item"]).sum() #sums the resale values by item here into it's own table
-
-with open('ResalebyItem.txt', 'w') as f: 
-    f.write(str(dataItems))
-
-# ------------------DISPLAYING DATA SUMMARY
-
+data["Resale Value"] = data["Resale Value"].str.replace('$','').str.replace(',','').astype(float) #remove format from # values used in calc
 data['Resale Difference'] = data['Resale Difference'].str.replace('$','').str.replace(',','').astype(float)
-totaldiff = data['Resale Difference'].sum()
-print("\nData Summary\nTotal Sales: ", data['Qty'].sum(), "\nTotal Resale Value: ", total, "\nTotal Resale Profit: ", totaldiff)
+# -------------------WORKING THE DATA
+total = data["Resale Value"].sum() #sums total resale values 
+with open('TotalResale.txt', 'w') as f: #outputs the total resale value answer in a .txt file
+    f.write(str(total))
+dataItems = data[["Item", "Resale Value"]].groupby(["Item"]).sum() #sums the resale values by item here into it's own table
+with open('ResalebyItem.txt', 'w') as f: #outputs the resale sums by item to a .txt table
+    f.write(str(dataItems))
+# ------------------DISPLAYING DATA SUMMARY
+print("\nData Summary\nTotal Quantity Sold: ", data['Qty'].sum(), "\nTotal Resale Value: ", total, "\nTotal Resale Profit: ", data['Resale Difference'].sum(), "\nTotal Resale Value per Item:\n\n", dataItems)
