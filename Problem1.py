189dcd7a78c343f598f91edb0a9c53830b0c61c5
Dylan Pike <107321662+Dtpike02@users.noreply.github.com>
2022-10-18 16:35:23 -0400

    Add files via upload

---
+++ Problem1.py
@@ -0,0 +1,18 @@
+from multiprocessing.sharedctypes import Value
+from operator import truediv
+import re
+import pandas as pd
+import csv
+
+
+
+
+def total_resale():
+    data = pd.read_csv("https://raw.githubusercontent.com/DanRossLi/TestProblem/main/TestData.csv")
+    data_float = data['Resale Value'].str.replace('$','', regex=True).replace(',', '', regex=True).astype('double')
+    total = data_float.sum()
+    print('Total Price of Resale is: $', total)
+
+
+if __name__ == '__main__':
+    total_resale()
